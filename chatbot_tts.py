79c38dacc46d2d0c66a7a41e1a724e3b4ee867b8
K.J. Chen <kjchen@pm.me>
2019-06-19 14:27:35 +0800

    Fixed BUG

--- chatbot_tts.py
+++ chatbot_tts.py
@@ -12,10 +12,13 @@ def chat_en():
 
     while 1:
         say = input('Please say something >>')
+
         rep = str(cb.get_response(say))
         tts = gTTS(text = rep, lang = 'en')
+
         time_now = time.asctime(time.localtime(time.time()))
         filename = 'Response on ' + time_now + '.mp3'
+
         tts.save(filename)
         os.system("mpg321 " + filename)
 
@@ -26,19 +29,26 @@ def chat_hant():
 
     while 1:
         say = input('Please say something >>')
+
         rep = str(cb.get_response(say))
         tts = gTTS(text = rep, lang = 'zh')
+
         time_now = time.asctime(time.localtime(time.time()))
         filename = 'Response on ' + time_now + '.mp3'
+
         tts.save(filename)
         os.system("mpg321 " + filename)
 
 def main():
-    lan = [["en", "EN", "English", "english", "ENGLISH"], ["hant", "zh-tw", "zh", "cn", "traditional-chinese", "chinese", "Chinese", "CHINESE"]]
+    lan_en = ["en", "EN", "English", "english", "ENGLISH"]
+    lan_hant = ["hant", "zh-tw", "zh", "cn", "traditional-chinese", "chinese", "Chinese", "CHINESE"]
+
     tmp = input('Please Enter Your Language >>')
+
     try:
-        chat_en() if tmp in lan[0]
-        chat_hant() if tmp in lan[1]
+        chat_en() if tmp in lan_en
+        chat_hant() if tmp in lan_hant
+
         print("=====END=====") else
     except:
         main()
